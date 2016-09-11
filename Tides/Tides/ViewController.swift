4f7f90dcfd6b8e1a347dc4589a1db0580b6e7ce6
Brian Blanco <brianblanco1@gmail.com>
2016-09-11 09:27:33 -0400

    Initial project created

---
+++ Tides/Tides/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tides
+//
+//  Created by Brian Blanco on 9/11/16.
+//  Copyright © 2016 Brian Blancco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
